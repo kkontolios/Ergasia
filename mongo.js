8b25f2d67637c13658391071d4a8777865111df2
kkontolios <158570701+kkontolios@users.noreply.github.com>
2024-02-02 11:23:17 +0000

    1

--- mongo.js
+++ mongo.js
@@ -1,44 +1,36 @@
-const db = "mongodb+srv://dimitris:p4w3Q123@cluster0.xjong1l.mongodb.net/?retryWrites=true&w=majority";
+const db ="mongodb+srv://kkontolios:<4HynY9BkQWKSCWo8>@cluster0.zq0xeex.mongodb.net/?retryWrites=true&w=majority";
 const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 const toId = mongoose.Types.ObjectId;
 
-mongoose.set('strictQuery',false);
-try
-{
-    mongoose.connect(db);
-    console.log("Connected to MongoDB");
-    
-}
-catch(error)
-{
-    console.error(error);
+mongoose.set("strictQuery", false);
+try {
+  mongoose.connect(db);
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error(error);
 }
 
 const LogInSchema = new mongoose.Schema({
-    username:
-    {
-        type:String,
-        required: true
-    },
-    email:
-    {
-        type:String,
-        required: true
-    },
-    password:
-    {
-        type: String,
-        required:true
-    },
-    permission:
-    {
-        type:String,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  permission: {
+    type: String,
+    required: true,
+  },
 });
 
-const collection = new mongoose.model("users",LogInSchema);
+const collection = new mongoose.model("users", LogInSchema);
 module.exports = collection;
 //console.log(mongoose.Schema.ObjectId.user);
